e343c08c28f7e8de2564a7e36c0a6a194cb914cd
Vita Tri Utami <110320983+vitatriutami@users.noreply.github.com>
2024-08-21 11:01:50 +0700

    fix: add dotenv

--- src/server.js
+++ src/server.js
@@ -9,8 +9,8 @@ const init = async () => {
   const notesService = new NotesService();
 
   const server = Hapi.server({
-    port: 3000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
